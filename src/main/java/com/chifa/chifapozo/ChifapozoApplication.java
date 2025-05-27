5dac0cf97f4bcda3b8d3fdc81ef05c152afe508d
AndSyncX <fernandezp.joe@gmail.com>
2025-05-26 23:38:11 -0500

    Iniciando Proyecto

---
+++ src/main/java/com/chifa/chifapozo/ChifapozoApplication.java
@@ -0,0 +1,13 @@
+package com.chifa.chifapozo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChifapozoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChifapozoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chifa/chifapozo/ChifapozoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chifa.chifapozo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChifapozoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
